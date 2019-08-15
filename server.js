279c5d31dbd1b275296172641f5a4b204b1cb9b1
WildestDeuce <48850844+WildestDeuce@users.noreply.github.com>
2019-08-14 20:06:30 -0400

    survey html now has 10 questions and submit button

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ let path = require("path");
 let app = express();
 var PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 require("./routing/apiRoutes")(app);
 require("./routing/htmlRoutes")(app);
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-  });
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
 

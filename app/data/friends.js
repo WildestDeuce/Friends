0cfd78738ed284e210e2664d6f7789362a3b080e
WildestDeuce <48850844+WildestDeuce@users.noreply.github.com>
2019-08-18 14:32:20 -0400

    href links added to home.html for loop added to apiRoutes.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -15,7 +15,41 @@ var friendsArray = [
             4,
             1
         ]
-    }
+    },
+
+    {
+        "name": "Debbie D.",
+        "photo": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fus.123rf.com%2F450wm%2Falvincadiz%2Falvincadiz1604%2Falvincadiz160400358%2F56426319-stock-vector-vector-illustration-of-smiley-emoticon-sad-face.jpg%3Fver%3D6&imgrefurl=https%3A%2F%2Fwww.123rf.com%2Fstock-photo%2Fsad_face.html&docid=o184kIs4VrFHRM&tbnid=aCZ4R_rekPB7TM%3A&vet=10ahUKEwiBgvGjrYrkAhWRdd8KHRmBBT8QMwhxKAAwAA..i&w=450&h=450&safe=strict&bih=609&biw=1423&q=images%20of%20sad%20faces&ved=0ahUKEwiBgvGjrYrkAhWRdd8KHRmBBT8QMwhxKAAwAA&iact=mrc&uact=8",
+        "scores": [
+            1,
+            1,
+            1,
+            2,
+            3,
+            1,
+            2,
+            1,
+            1,
+            1
+        ]
+    },  
+    
+    {
+        "name": "Mr EIA.",
+        "photo": "https://www.google.com/search?q=image+super+happy+face&safe=active&rlz=1C1CHBF_enUS841US841&tbm=isch&source=iu&ictx=1&fir=JBBDgUwSGhgwmM%253A%252CZczl6Q0DyqJGhM%252C_&vet=1&usg=AI4_-kTIoiBLZciOzDYy1nHL90Jb6_aehw&sa=X&ved=2ahUKEwjc7K2ArorkAhUoWN8KHUO3DjEQ9QEwAXoECAkQCQ#imgrc=JBBDgUwSGhgwmM:",
+        "scores": [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },    
 ]
 
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,19 @@
 //A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
 let friends = require('../data/friends');
 
-module.exports = function(app){
-    app.get('/api/friends', function(req, res) {
+module.exports = function (app) {
+    app.get('/api/friends', function (req, res) {
         res.json(friends);
     })
-    app.post('/api/friends', function(req, res){
+
+    app.post('/api/friends', function (req, res) {
+        friends.push(req.body)
         res.json(true);
+        for (var i = 0; i < friendsHolder.length; i++) {
+            for (var j = 0; j < friendsHolder.scores[j].length; j++) {
+
+                Math.abs(value)
+            }
+        }
     })
 };

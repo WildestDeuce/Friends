9de4ade6e2a40e13ab023014dff6087ab657a240
WildestDeuce <48850844+WildestDeuce@users.noreply.github.com>
2019-08-18 21:49:32 -0400

    api routes logic added for absolute value comparison of data

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,10 +9,29 @@ module.exports = function (app) {
     app.post('/api/friends', function (req, res) {
         friends.push(req.body)
         res.json(true);
+        var match = 0;
+        var absoluteValueArray = [];
+        var compare = 0;
         for (var i = 0; i < friendsHolder.length; i++) {
             for (var j = 0; j < friendsHolder.scores[j].length; j++) {
+                var num1 = friendsHolder[i].scores[j];
+                var num2 = req.body.scores[j];
+                var absVal = Math.abs(num1 - num2);
 
-                Math.abs(value)
+                compare += absVal;
+            }
+            friendsHolder[i].absVal = parseInt(compare);
+            absoluteValueArray.push(parseInt(compare));
+
+            console.log(`Person: ${friendsHolder[i].name}\nAbosoluteValue: ${friendsHolder[i].absVal}\n`)
+        }
+        absoluteValueArray.sort();
+        console.log(`Sorted:${absoluteValueArray[0]}`)
+        var minimum = Math.min(...absoluteValueArray)
+
+        for (k = 0; k < friendsHolder.length; k++) {
+            if (parseInt(minimum === friendsHolder[k].absVal){
+                match = friendsHolder[k];
             }
         }
     })

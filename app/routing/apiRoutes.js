4721e200ae676a6f1d224ac6f20dabb62f0d15de
WildestDeuce <48850844+WildestDeuce@users.noreply.github.com>
2019-08-20 21:08:00 -0400

    logic completed for friends comparison modal pushes to browser

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,19 +1,17 @@
 //A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
-let friends = require('../data/friends');
+let friendsHolder = require('../data/friends');
 
 module.exports = function (app) {
     app.get('/api/friends', function (req, res) {
-        res.json(friends);
+        res.json(friendsHolder);
     })
 
     app.post('/api/friends', function (req, res) {
-        friends.push(req.body)
-        res.json(true);
         var match = 0;
         var absoluteValueArray = [];
         var compare = 0;
         for (var i = 0; i < friendsHolder.length; i++) {
-            for (var j = 0; j < friendsHolder.scores[j].length; j++) {
+            for (var j = 0; j < friendsHolder[i].scores.length; j++) {
                 var num1 = friendsHolder[i].scores[j];
                 var num2 = req.body.scores[j];
                 var absVal = Math.abs(num1 - num2);
@@ -23,16 +21,20 @@ module.exports = function (app) {
             friendsHolder[i].absVal = parseInt(compare);
             absoluteValueArray.push(parseInt(compare));
 
-            console.log(`Person: ${friendsHolder[i].name}\nAbosoluteValue: ${friendsHolder[i].absVal}\n`)
+            console.log(`Person: ${friendsHolder[i].name}\nAbsoluteValue: ${friendsHolder[i].absVal}\n`)
         }
-        absoluteValueArray.sort();
-        console.log(`Sorted:${absoluteValueArray[0]}`)
+        
         var minimum = Math.min(...absoluteValueArray)
 
         for (k = 0; k < friendsHolder.length; k++) {
-            if (parseInt(minimum === friendsHolder[k].absVal){
+            if (parseInt(minimum) === friendsHolder[k].absVal){
                 match = friendsHolder[k];
             }
         }
+        console.log(match);
+        friendsHolder.push(req.body);
+        res.json(match);
     })
+
+    friendsHolder.push(req.body)
 };
